7b9f4256ccd656993ec42540314d8afcd02b7712
babicevl <70582524+babicevl@users.noreply.github.com>
2020-10-27 14:33:51 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
 // JavaScript Document
+
+var button = document.querySelector("button");
+var hart = document.querySelector("div");
+let btn = document.querySelector('button');
+let classObj = {
+  original: 'large',
+  // small: 'medium',
+  // medium: 'large',
+  large: 'original'
+}
+
+// function
+
+function likeButton () {
+	hart.classList.toggle ("original");
+}
+
+console.log(button);
+
+// eventlistener
+
+addEventListener('click', likeButton);
+	console.log("button was clicked");
+
+
+
+btn.addEventListener('click',()=>{
+  btn.classList = classObj[btn.classList[0]];
+});
+
+ 
